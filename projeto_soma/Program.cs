072059f4d02d0934954f65eefce25307b96b77f6
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-10-22 21:55:28 -0400

    meu commit de soma

---
+++ projeto_soma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace projeto_soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor_01 = 4;
+            int valor_02 = 2;
+
+            Console.WriteLine("A soma e igual a: " + (valor_01 + valor_02));
+
+            Console.WriteLine("A subtracao e igual a: " + (valor_01 - valor_02));
+
+            Console.WriteLine("A multiplicacao e igual a: " + (valor_01 * valor_02));
+
+            Console.WriteLine("A divisao e igual a: " + (valor_01 / valor_02));
+
+        }
+    }
+}
